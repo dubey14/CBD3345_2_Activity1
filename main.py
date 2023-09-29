e9d7139038bf3803e9834164d30539c0d326c5b4
Tanay Van <tanayvan@Tanays-MacBook-Pro.local>
2023-09-29 09:58:20 -0400

    added flask

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
 from src.data_preparation import generate_regression_data
 from src.model import create_regression_model
 from src.train import train_regression_model
 from src.predict import predict_regression
 
-# Generate synthetic regression data
-X_train, y_train = generate_regression_data()
+app = Flask(__name__)
 
-# Create and train the regression model
-regression_model = create_regression_model(X_train.shape[1:])
-train_regression_model(regression_model, X_train, y_train, epochs=100, batch_size=32)
+# Define your fixtures and model loading here
 
-# Perform regression prediction
-sample_input = [[0.5]]  # Example input for prediction
-predicted_value = predict_regression(regression_model, sample_input)
-print(f"Predicted value: {predicted_value[0][0]}")
+@app.route('/')
+def hello():
+    return 'Welcome to the Regression Model API!'
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    # sample_input = [[0.5]]  
+    sample_input = np.random.rand(1, 1)# Example input for prediction
+    predicted_value = predict_regression(model, sample_input)
+    print(predicted_value)
+    # return jsonify({'predicted_value': predicted_value.tolist()})
+    html_response = f"<h1>Predicted Value: {predicted_value[0][0]}</h1>"
+
+    return html_response
+
+if __name__ == '__main__':
+    # Load your regression model here
+    X_train, y_train = generate_regression_data()
+    model = create_regression_model(X_train.shape[1:])
+    train_regression_model(model, X_train, y_train, epochs=1, batch_size=32)
+    
+    app.run(debug=True)
