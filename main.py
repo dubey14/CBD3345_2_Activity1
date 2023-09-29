052a6a34841ab27837587457c68e43f1c55aed9a
Tanay Van <tanayvan@Tanays-MacBook-Pro.local>
2023-09-29 13:05:03 -0400

    final commit to shut anikets mouth

--- main.py
+++ main.py
@@ -11,14 +11,23 @@ app = Flask(__name__)
 
 # Define your fixtures and model loading here
 
+# Define the model globally so it can be accessed by the predict function
+model = None
+X_train, y_train = generate_regression_data()
+model = create_regression_model(X_train.shape[1:])
+train_regression_model(model, X_train, y_train, epochs=1, batch_size=32)
 @app.route('/')
 def hello():
     return 'Welcome to the Regression Model API!'
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    # sample_input = [[0.5]]  
-    sample_input = np.random.rand(1, 1)# Example input for prediction
+    global model  # Access the globally defined model
+    if model is None:
+        return "Model is not loaded. Please load the model first."
+
+    # sample_input = [[0.5]]
+    sample_input = np.random.rand(1, 1)  # Example input for prediction
     predicted_value = predict_regression(model, sample_input)
     print(predicted_value)
     # return jsonify({'predicted_value': predicted_value.tolist()})
@@ -28,8 +37,6 @@ def predict():
 
 if __name__ == '__main__':
     # Load your regression model here
-    X_train, y_train = generate_regression_data()
-    model = create_regression_model(X_train.shape[1:])
-    train_regression_model(model, X_train, y_train, epochs=1, batch_size=32)
     
+
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
